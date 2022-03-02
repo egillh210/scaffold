56973f4e7308eb4d66041b137f26692fe692dee7
Egill Hreinsson <egillh210@gmail.com>
2022-03-02 16:52:25 +0000

    bla

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -8,6 +8,12 @@ import "hardhat/console.sol";
 contract YourContract {
 
   event SetPurpose(address sender, string purpose);
+  // event PositionUpserted(address indexed trader,
+  //       uint indexed positionId,
+  //       Position position,
+  //       int protocolFees,
+  //       int realisedPnL
+  //   );
 
   string public purpose = "Building Unstoppable Apps!!!";
 
@@ -24,4 +30,14 @@ contract YourContract {
   // to support receiving ETH by default
   receive() external payable {}
   fallback() external payable {}
+
+  // struct Position {
+  //   int128 collateral; // User collateral
+  //   int128 openQuantity; // total quantity to which the trader is exposed, positive for long, negative for short, always in base ccy
+  //   int128 openCost; // total amount that the trader exchanged for base, positive for short, negative for long, always in quote ccy
+  //   int128 protocolFees; // fees this position owes, this field is WIP and may be dropped
+  //   uint8 leverage; // max leverage that was valid when this position was opened
+  //   uint32 maturity;
+  //   bytes1 liquiditySource;
+  // }
 }
